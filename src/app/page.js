4ab75ae4427cab74107d1c6fc1cd87cb49b00c74
Ara Bella <arabella@Aras-MacBook-Pro.local>
2025-01-24 14:02:06 -0800

    create and style header

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "./components/Header/Header";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Header/>
       <main className={styles.main}>
         <h1>Arabella</h1>
         <h1>Alegria</h1>
