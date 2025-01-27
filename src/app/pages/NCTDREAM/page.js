e8e9d3e8e6380148dd22ec8a588e6c99aa89c820
Ara Bella <arabella@Aras-MacBook-Pro.local>
2025-01-27 02:06:15 -0800

    add image and some styling in nct dream page

--- src/app/pages/NCTDREAM/page.js
+++ src/app/pages/NCTDREAM/page.js
@@ -11,13 +11,31 @@ export default function NCTDREAM() {
                     <h1>DREAMville</h1>
                     <p>Wordpress</p>
                     <Image
-                        src="/nctdream_bnw.jpeg"
+                        src="/nctdream_banner.jpeg"
                         height={600}
                         width={900}
                         alt=""
                     />
                     <p>DREAMville is a platform dedicated to introducing NCT Dream to both new and longtime fans. It offers everything that newcomers need to dive into their world, from inspiring milestones and achievements to their story of their journey as artists. Beyond their music and discography, DREAMvile also highlights each member, providing a deeper connection to the group and their history.
                     </p>
+                    <Image
+                        src="/dreamville1.jpeg"
+                        height={500}
+                        width={1000}
+                        alt="dreamville landing page screenshot"
+                    />
+                    <Image
+                        src="/dreamville2.png"
+                        height={500}
+                        width={1000}
+                        alt="dreamville profile page screenshot"
+                    />
+                    <Image
+                        src="/dreamville3.png"
+                        height={500}
+                        width={1000}
+                        alt="dreamville blog page screenshot"
+                    />
                 </main>
             </div>
         </>
