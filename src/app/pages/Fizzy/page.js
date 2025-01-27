7a05e81db1b7d8f2fb286f597b6b374098cecb95
Ara Bella <arabella@Aras-MacBook-Pro.local>
2025-01-27 01:34:11 -0800

    add text and images in Fizzy page

--- src/app/pages/Fizzy/page.js
+++ src/app/pages/Fizzy/page.js
@@ -1,11 +1,55 @@
 import styles from "./Fizzy.module.css";
+import Header from "@/app/components/Header/Header";
+import Image from "next/image";
 
 export default function Fizzy() {
     return (
         <>
             <div className={styles.page}>
+                <Header/>
                 <main className={styles.main}>
-
+                    <div className={styles.banner}>
+                        <div className={styles.bannerText}>
+                            <h1>Fizzy</h1>
+                            <p>Adobe Illustrator | Branding | Packaging</p>
+                            <p>
+                                Fizzy is a sparkling aid series that offers the flavour of berries, lemon hibiscus, and grapes. 
+                                Its playful, fun design reflects the refreshing taste of each time period, from the morning to midnight. 
+                                Perfect to drink in any occasion, Fizzy is your go-to for a delightful burst of flavour.
+                            </p>
+                        </div>
+                        <Image
+                            src="/Fizzy_Flavours.png"
+                            width={800}
+                            height={600}
+                            alt="Fizzy's Oceanade Berries Can Mockup"
+                            className={styles.can}
+                        />
+                    </div>
+                    <p>Scroll down to see more</p>
+                    <div className={styles.fizzyDesigns}>
+                        <Image
+                            src="/Oceanade_Berries.svg"
+                            width={900}
+                            height={900}
+                            alt="Fizzy's Oceanade Berries Full Design"
+                            className={styles.Oceanade}
+                        />
+                        <Image
+                            src="/Sunset_Splash.svg"
+                            width={900}
+                            height={900}
+                            alt="Fizzy's Sunset Splash Full Design"
+                            className={styles.Sunset}
+                        />
+                        <Image
+                            src="/Midnight_Current.svg"
+                            width={900}
+                            height={900}
+                            alt="Fizzy's Midnight Current Full Design"
+                            className={styles.Midnight}
+                        />
+                    </div>
                 </main>
             </div>
         </>
