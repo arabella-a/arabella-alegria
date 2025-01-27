031596882fe1cae57c35f5956add64115a5f9e01
Ara Bella <arabella@Aras-MacBook-Pro.local>
2025-01-27 00:59:53 -0800

    add fizzy page

---
+++ src/app/pages/Fizzy/page.js
@@ -0,0 +1,13 @@
+import styles from "./Fizzy.module.css";
+
+export default function Fizzy() {
+    return (
+        <>
+            <div className={styles.page}>
+                <main className={styles.main}>
+
+                </main>
+            </div>
+        </>
+    )
+}
