6cd1f71e23958d0afaa7af8301bda9d30cb7851f
Ara Bella <arabella@Aras-MacBook-Pro.local>
2025-01-27 01:42:53 -0800

    add nct dream page. remove a section in fizzy css file

---
+++ src/app/pages/NCTDREAM/page.js
@@ -0,0 +1,13 @@
+import styles from "./nctDream.module.css";
+
+export default function NCTDREAM() {
+    return (
+        <>
+            <div className={styles.page}>
+                <main className={styles.main}>
+                    <h1> hi hello nct dream </h1>
+                </main>
+            </div>
+        </>
+    )
+}
