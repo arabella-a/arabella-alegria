296aceb3a4b13c89fa32a627bec974b88ba99acb
arabella-a <aramdl.alegria02@gmail.com>
2025-02-01 13:43:09 -0800

    add description and images. some styling

--- src/app/pages/Ningning/page.js
+++ src/app/pages/Ningning/page.js
@@ -11,8 +11,14 @@ export default function Ningning() {
                     <div className={styles.banner}>
                         <div className={styles.bannerText}>
                             <h1>aespa NINGNING</h1>
-                            <p>Photoshop</p>
-                            <p>words words words</p>
+                            <p>Adobe Photoshop | Adobe Illustrator</p>
+                            <p>
+                                With aespa's highly anticipated launch, individual posters are the perfect way to promote their Japanese single, <span>Hot Mess</span>!  
+                                Its design takes inspiration from iconic Y2K Japanese magazine covers, paying tribute to the release of their Japanese debut. 
+                                All elements in this poster are thought thoroughly. From its colours to the languages used. 
+
+                                As this is going to be released to the Japanese public, both English and Katakana are used. 
+                            </p>
                         </div>
                         <div>
                             <Image 
@@ -23,7 +29,30 @@ export default function Ningning() {
                             />
                         </div>
                     </div>
-                    <p className={styles.scroll}>Scroll down to see how it fits into the real world!</p>
+                    <p className={styles.scroll}>Scroll down to see how it fits into the real world! <span>&#8595;</span></p>
+                    <section className={styles.gallery}>
+                        <h3>Real-Life Scenarios</h3>
+                        <p>
+                            Here's the poster on a wall where people often pass by. 
+                            With its bright and fun look, it's hard to miss! &#40; Even the dog seems to like it 🫣&#41;
+                        </p>
+                        <Image
+                            src="/ningning_poster_wall.jpg"
+                            width={1000}
+                            height={675}
+                            alt="ninging's poster on a wall"
+                        />
+                        <p>
+                            This poster isn't just wall-friendly—it's perfect for OOH spaces too. 
+                            In the busy streets, this poster is eye-catching! 
+                        </p>
+                        <Image
+                            src="/ningning_poster_billboard.jpg"
+                            width={1000}
+                            height={650}
+                            alt="ninging's poster on a billboard outside a building"
+                        />
+                    </section>
                 </main>
             </div>
         </>
