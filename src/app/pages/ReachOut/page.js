dc6d2b105a2eae3252e7ff97952a6f640d8f804b
arabella-a <aramdl.alegria02@gmail.com>
2025-02-10 12:09:09 -0800

    add reachout page to development page

---
+++ src/app/pages/ReachOut/page.js
@@ -0,0 +1,15 @@
+import styles from "./ReachOut.module.css";
+import Header from "@/app/components/Header/Header";
+
+export default function ReachOut() {
+    return (
+        <>
+            <Header/>
+            <div className={styles.pages}>
+                <main className={styles.main}>
+
+                </main>
+            </div>
+        </>
+    )
+}
