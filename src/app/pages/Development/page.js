3bf9525ecac0aeb1e50c035cfd1db15b242a9a3f
arabella-a <aramdl.alegria02@gmail.com>
2025-02-28 16:49:31 -0800

    add new content

--- src/app/pages/Development/page.js
+++ src/app/pages/Development/page.js
@@ -2,6 +2,7 @@ import Header from "@/app/components/Header/Header";
 import Filter from "@/app/components/Filter/Filter";
 import styles from "./Development.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Development() {
     return (
@@ -11,6 +12,48 @@ export default function Development() {
                 <main className={styles.main}>
                     <h1>Works</h1>
                     <Filter/>
+                    <section className={styles.works}>
+                        <div className={styles.lineOne}>
+                            <div className={styles.banner}>
+                                    <Image 
+                                        src={"/dreamlaptop.jpeg"}
+                                        width={380}
+                                        height={550}
+                                        alt="dreamville on laptop mockup"
+                                    />
+                                    <div className={styles.bannerInformation}>
+                                        <h3>DREAMville</h3>
+                                        <Link href="/pages/NCTDREAM"><p>View Project</p></Link>
+                                    </div>
+                            </div>
+                            <div className={styles.banner}>
+                                    <Image 
+                                        src={"/reachout-cut.jpeg"}
+                                        width={380}
+                                        height={550}
+                                        alt="reachout on laptop mockup"
+                                    />
+                                    <div className={styles.bannerInformation}>
+                                        <h3>ReachOut</h3>
+                                        <Link href="/pages/ReachOut"><p>View Project</p></Link>
+                                    </div>
+                            </div>
+                        </div>
+                        <div className={styles.lineTwo}>
+                            <div className={styles.banner}>
+                                <Image 
+                                    src={"/hivefivephone.jpeg"}
+                                    width={380}
+                                    height={550}
+                                    alt="hivefive on phone mockup"
+                                />
+                                <div className={styles.bannerInformation}>
+                                    <h3>ReachOut</h3>
+                                    <Link href="/"><p>View Project</p></Link>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </main>
             </div>
         </>
