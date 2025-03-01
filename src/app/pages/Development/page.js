3fbdb2f0a386daf932cf1337c44029fbab8d3333
arabella-a <aramdl.alegria02@gmail.com>
2025-02-28 16:26:36 -0800

    clear content

--- src/app/pages/Development/page.js
+++ src/app/pages/Development/page.js
@@ -11,10 +11,6 @@ export default function Development() {
                 <main className={styles.main}>
                     <h1>Works</h1>
                     <Filter/>
-                    <Link href="/pages/NCTDREAM"><div className={styles.nctDreamCard}></div></Link>
-                    <Link href="/pages/ReachOut"><div className={styles.reachOutCard}></div></Link>
-                    <div className={styles.Card}></div>
-                    <div className={styles.Card}></div>
                 </main>
             </div>
         </>
