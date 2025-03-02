063858695ebf15f52cc949f4fc1ecb34b455c626
arabella-a <aramdl.alegria02@gmail.com>
2025-03-02 14:02:02 -0800

    add banner and role

--- src/app/pages/ReachOut/page.js
+++ src/app/pages/ReachOut/page.js
@@ -9,6 +9,22 @@ export default function ReachOut() {
             <div className={styles.pages}>
                 <main className={styles.main}>
                     <Header/>
+                    <div className={styles.imgContainer}>
+                        <Image 
+                            src={"/reachout-banner.jpeg"}
+                            width={1450}
+                            height={850}
+                            alt="reachout on laptop mockup with white background"
+                        />
+                        <div className={styles.overlay}></div>
+                    </div>
+                    <div className={styles.information}>
+                        <ul>
+                            <li>Figma | Premiere Pro</li>
+                            <li>UI Designer | Video Editor</li>
+                            <li>December 2024</li>
+                        </ul>
+                    </div>
                 </main>
             </div>
         </>
