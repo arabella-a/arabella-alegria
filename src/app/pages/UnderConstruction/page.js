7d1ed0a8753372925b0e15cc0c8e56982dfa1728
arabella-a <aramdl.alegria02@gmail.com>
2025-03-02 23:32:12 -0800

    add under construction page

---
+++ src/app/pages/UnderConstruction/page.js
@@ -0,0 +1,11 @@
+export default function UnderConstruction() {
+    return (
+        <>
+            <div>
+                <main>
+                    <h1>Hi!! 👷‍♀️ This page is currently under construction 🚧 .... 🚧 Please come back later..? 🫣</h1>
+                </main>
+            </div>
+        </>
+    )
+}
