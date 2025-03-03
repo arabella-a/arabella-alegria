059bc6bc8a92a3982f90a26ef7bdb2f80e767e87
arabella-a <aramdl.alegria02@gmail.com>
2025-03-02 23:39:16 -0800

    link construction page and style

--- src/app/pages/UnderConstruction/page.js
+++ src/app/pages/UnderConstruction/page.js
@@ -1,8 +1,12 @@
+import styles from "./UnderConstruction.module.css";
+import Header from "@/app/components/Header/Header";
+
 export default function UnderConstruction() {
     return (
         <>
-            <div>
-                <main>
+            <div className={styles.page}>
+                <Header />
+                <main className={styles.main}>
                     <h1>Hi!! 👷‍♀️ This page is currently under construction 🚧 .... 🚧 Please come back later..? 🫣</h1>
                 </main>
             </div>
