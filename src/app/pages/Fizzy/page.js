e785d0c8a6a52e4d58689d936afc8496a783812c
arabella-a <aramdl.alegria02@gmail.com>
2025-03-05 11:30:12 -0800

    add about section and create dielineContainer

--- src/app/pages/Fizzy/page.js
+++ src/app/pages/Fizzy/page.js
@@ -11,6 +11,78 @@ export default function Fizzy() {
                 <div className={styles.backBtn}>
                 </div>
                 <main className={styles.main}>
+                    <div className={styles.imgContainer}>
+                        <Image 
+                            src={"/fizzy-banner.jpeg"}
+                            layout="responsive"
+                            width={100}
+                            height={850}
+                            alt="three fizzy cans the their flavours splayed out on the floor"
+                        />
+                        <div className={styles.overlay}></div>
+                    </div>
+                    <section className={styles.about}>
+                        <div className={styles.information}>
+                            <ul>
+                                <li>Adobe Illustrator</li>
+                                <li>Graphic Design | Branding</li>
+                                <li>November 2024</li>
+                            </ul>
+                        </div>
+                        <div className={styles.aboutContainer}>
+                            <Image 
+                                src={"/Fizzy_Flavours.png"}
+                                width={205}
+                                height={450}
+                                alt="fizzy oceanade berries can mockup"
+                            />
+                            <div className={styles.aboutInfo}>
+                                <h2>About <span>fizzy</span></h2>
+                                <p>
+                                    <span>fizzy</span> is a sparkling aid series that offers the flavour of berries, lemon hibiscus, and grapes. 
+                                    Its playful, fun design reflects the refreshing taste of each time period, from the morning to midnight. 
+                                    Perfect to drink in any occasion, fizzy is your go-to for a delightful burst of flavour.
+                                </p>
+                            </div>
+                        </div>
+                        <div className={styles.targetMarketContainer}>
+                            <div className={styles.targetMarketInfo}>
+                                <h2>Who its for</h2>
+                                <p>
+                                    <span>fizzy</span> is designed for young adults who enjoy a refreshing drink with a playful twist. 
+                                    Whether they’re hanging out with friends, partying, or on the road, they appreciate the bold flavours and eye-catching design of the drink! 
+                                    The fun-loving otter mascot and the vibrant packaging bring a sense of joy, making <span>fizzy</span> a drink that stands out and sparks conversation.
+                                </p>
+                            </div>
+                            <Image 
+                                src={"/fizzy_flavour2.png"}
+                                width={205}
+                                height={450}
+                                alt="fizzy sunset splash can mockup"
+                            />
+                        </div>
+                        <div className={styles.experienceContainer}>
+                        <Image 
+                                    src={"/fizzy_flavour3.png"}
+                                    width={205}
+                                    height={450}
+                                    alt="fizzy sunset splash can mockup"
+                                />
+                            <div className={styles.experienceInfo}>
+                                <h2>The <span>fizzy</span> Experience</h2>
+                                <p>
+                                    The design for <span>fizzy</span> reflect its fun and energetic personality. 
+                                    Its bright, and bold colours create an eye-catching look that will appeal to the youthful audience. 
+                                    Not only that but the otter adds a sense of charm and approachability.  
+                                    The mascot is a big part of <span>fizzy</span>’s identity, hugging different fruits depending on the flavour, 
+                                    with a variety of happy expressions that make each can feel unique and full of character.
+                                </p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.dielineContainer}>
+
+                    </section>
                 </main>
             </div>
         </>
