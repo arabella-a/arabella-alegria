fdf6c3a1fc903ddc4d1e0d83c24551cce500d11e
arabella-a <aramdl.alegria02@gmail.com>
2025-03-16 20:29:56 -0700

    add img container

--- src/app/pages/Ningning/page.js
+++ src/app/pages/Ningning/page.js
@@ -8,7 +8,19 @@ export default function Ningning() {
             <div className={styles.page}>
                 <Header />
                 <main className={styles.main}>
-                    
+                    <div className={styles.imgContainer}>
+                        <Image 
+                            src={"/ningning_poster_wall.jpg"}
+                            layout="responsive"
+                            width={100}
+                            height={850}
+                            alt="ningning poster on a wall"
+                        />
+                        <div className={styles.overlay}></div>
+                    </div>
+                    <section className={styles.about}>
+
+                    </section>
                 </main>
             </div>
         </>
