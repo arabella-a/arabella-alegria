d2d36bbba8fe98eda8b66376e6c4a689c76292ed
arabella-a <aramdl.alegria02@gmail.com>
2025-03-16 21:56:49 -0700

    add about section

--- src/app/pages/NCTDREAM/page.js
+++ src/app/pages/NCTDREAM/page.js
@@ -8,7 +8,40 @@ export default function NCTDREAM() {
             <div className={styles.page}>
                 <Header/>
                 <main className={styles.main}>
-                    
+                    <div className={styles.imgContainer}>
+                        <Image 
+                            src={"/dream_banner.png"}
+                            layout="responsive"
+                            width={100}
+                            height={100}
+                            alt="nct dream website on a laptop mockup"
+                        />
+                        <div className={styles.overlay}></div>
+                    </div>
+                    <section className={styles.about}>
+                        <div className={styles.information}>
+                            <ul>
+                                <li>WordPress</li>
+                                <li>UI/UX Design | Development</li>
+                                <li>March 2024</li>
+                            </ul>
+                        </div>
+                        <div className={styles.aboutContainer}>
+                            <Image 
+                                src={"/nctdream_mac.png"}
+                                width={400}
+                                height={300}
+                                alt="dreamville landing page on mac mockup"
+                            />
+                            <div>
+                                <h2>About DREAMville</h2>
+                                <p>
+                                    DREAMville is designed to celebrate the journey and achievements of NCT DREAM. 
+                                    By highlighting their history and music, DREAMville aims to introduce NCT DREAM to both new fans and long-time fans alike.
+                                </p>
+                            </div>
+                        </div>
+                    </section>
                 </main>
             </div>
         </>
