5f7a56e0c72750f6d59b8dffd0d0977ac7c5e88d
arabella-a <aramdl.alegria02@gmail.com>
2025-03-28 01:08:26 -0700

    add custom cursor in what to do page

--- src/app/pages/WhatToDo/page.js
+++ src/app/pages/WhatToDo/page.js
@@ -3,11 +3,51 @@ import Header from "@/app/components/Header/Header";
 import Image from "next/image";
 
 export default function WhatToDo() {
+
+    useEffect(() => {
+        const cursorDot = document.querySelector("[data-cursor-dot]");
+        const cursorButterfly = document.querySelector("[data-cursor-outline]");
+          
+        if (!cursorDot || !cursorButterfly) {
+            console.error("Cursor elements not found!");
+            return;
+        }
+          
+        const handleMouseMove = (e) => {
+        const posX = e.clientX;
+        const posY = e.clientY;
+          
+        console.log("Mouse position:", posX, posY); 
+          
+        cursorDot.style.left = `${posX}px`;
+        cursorDot.style.top = `${posY}px`;
+          
+        cursorButterfly.animate(
+            {
+                transform: `translate(${posX}px, ${posY}px)`,
+            },
+            { duration: 150, fill: "forwards" }
+        );
+        };
+          
+        window.addEventListener("mousemove", handleMouseMove);
+        return () => window.removeEventListener("mousemove", handleMouseMove);
+        }, []);
+        
     return (
         <>
             <div className={styles.page}>
                 <Header />
                 <main className={styles.main}>
+                    <div className={styles.cursorDot} data-cursor-dot></div>
+                    <div className={styles.cursorButterfly} data-cursor-outline>
+                        <Image 
+                        src={"/buttfly-1.svg"}
+                        width={50}
+                        height={50}
+                        alt="butterfly"
+                        />
+                    </div>
                     <section className={styles.imgContainer}>
                         <Image 
                             src={"/what_to_do_banner.jpeg"}
