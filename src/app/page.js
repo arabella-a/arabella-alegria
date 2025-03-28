41b48f786fe9aad74b4e0250b4d3dcc0e2c17ec2
arabella-a <aramdl.alegria02@gmail.com>
2025-03-28 00:42:32 -0700

    add butterfly cursor in main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,58 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/Header/Header";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
 
+    useEffect(() => {
+      const cursorDot = document.querySelector("[data-cursor-dot]");
+      const cursorButterfly = document.querySelector("[data-cursor-outline]");
+  
+      if (!cursorDot || !cursorButterfly) {
+        console.error("Cursor elements not found!");
+        return;
+      }
+  
+      const handleMouseMove = (e) => {
+        const posX = e.clientX;
+        const posY = e.clientY;
+  
+        console.log("Mouse position:", posX, posY); 
+  
+        cursorDot.style.left = `${posX}px`;
+        cursorDot.style.top = `${posY}px`;
+  
+        cursorButterfly.animate(
+          {
+            transform: `translate(${posX}px, ${posY}px)`,
+          },
+          { duration: 150, fill: "forwards" }
+        );
+      };
+  
+      window.addEventListener("mousemove", handleMouseMove);
+      return () => window.removeEventListener("mousemove", handleMouseMove);
+    }, []);
+  
+
   return (
     <div className={styles.page}>
-      <div className={styles.cursorDot} data-cursor-dot></div>
-      <div className={styles.cursorButterfly}></div>
+
       <Header/>
       <main className={styles.main}>
+      <div className={styles.cursorDot} data-cursor-dot></div>
+      <div className={styles.cursorButterfly} data-cursor-outline>
+        <Image 
+          src={"/buttfly-1.svg"}
+          width={50}
+          height={50}
+          alt="butterfly"
+        />
+      </div>
         <section className={styles.name}>
           <div className={styles.firstName}>
             <svg width="646" height="117" viewBox="0 0 646 117" fill="none" xmlns="http://www.w3.org/2000/svg">
